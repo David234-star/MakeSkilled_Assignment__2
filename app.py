181d0743c0f8bcd53732fa7fdcac94e135448fc4
B. Ranadeer <davidbasiripogu@hotmail.com>
2023-05-13 14:40:05 +0530

    changed from hello world to palindrome

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-n = input("Enter your name: ")
+def is_palindrome(string):
+    # convert to lowercase and remove non-alphabetic characters
+    string = "".join(c.lower() for c in string if c.isalpha())
+    # compare to its reverse
+    reversed_string = "".join(reversed(string))
+    return string == reversed_string
 
-print("Hello, World!",n)
+# get user input for the string
+user_input = input("Enter a string: ")
+
+# check if the string is a palindrome
+if is_palindrome(user_input):
+    print(f"{user_input} is a palindrome.")
+else:
+    print(f"{user_input} is not a palindrome.")
